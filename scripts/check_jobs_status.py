6e652be9109fa4eb07270179d2df5f31da65a281
Shujia Huang <huangshujia@Shujia-Huangs-iMac.local>
2020-04-25 16:42:50 +0800

    first commit

---
+++ scripts/check_jobs_status.py
@@ -0,0 +1,45 @@
+"""
+Author: Shujia Huang
+Date: 2020-04-25
+"""
+import re
+import os
+import sys
+
+pattern = re.compile(r'^\[\S+\]\s+(\S+)\s+done')
+
+
+def check(input_fname):
+    is_finish = False
+    if os.path.exists(input_fname):
+        with open(input_fname) as I:
+            lines = I.readlines()
+            last_line = lines[-1].strip()
+            match = pattern.match(last_line)
+
+            if match:
+                is_finish = True
+
+    return is_finish
+
+
+if __name__ == "__main__":
+
+    task_log_file = sys.argv[1]
+
+    is_all_finish = True
+    with open(task_log_file) as I:
+        for line in I:
+            # sample log_file task_shell_file"
+            if line.startswith("#"):
+                continue
+
+            sample, log_fname, task_shell = line.strip().split()
+            is_finish = check(log_fname)
+
+            if not is_finish:
+                is_all_finish = False
+                print ("[unfinish]%s\t%s" % (sample, task_shell))
+
+    if is_all_finish:
+        print ("** All Jobs done **")
