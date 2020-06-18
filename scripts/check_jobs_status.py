9e2ebea03f269bd2846a7ef934bac2d1893048b6
Shujia Huang <huangshujia9@gmail.com>
2020-06-18 14:30:17 +0800

    print->sys.stderr.write

--- scripts/check_jobs_status.py
+++ scripts/check_jobs_status.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
                 print ("[unfinish]\t%s\t%s" % (sample, task_shell))
 
     if is_all_finish:
-        print ("** All Jobs done **")
+        sys.stderr.write("** All Jobs done **\n")
