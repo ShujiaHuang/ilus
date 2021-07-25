84b70c4b84913dac5887d41b75ca0862cfa76921
Shujia Huang <huangshujia9@gmail.com>
2021-07-25 15:20:41 +0800

    使用新的包管理和发布方式

---
+++ release.py
@@ -0,0 +1,22 @@
+"""Release and publish geneview to PyPI.
+
+Author: Shujia Huang
+Date: 2021-04-30
+"""
+import importlib
+from subprocess import call
+
+spec = importlib.util.spec_from_file_location("_", "./setup.py")
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+# Publish ilus to PyPI by using twine: https://segmentfault.com/a/1190000008663126
+# pip install twine
+# python setup.py register
+# python setup.py sdist build && twine upload dist/ilus-0.0.5.tar.gz
+
+#call(["pandoc", "--from=markdown", "--to=rst", "-o", "README.rst", "README.md"])
+call(["python", "setup.py", "sdist"])
+tarball = "dist/{}-{}.tar.gz".format(module.meta.__DISTNAME__, module.meta.__VERSION__)
+call(["twine", "upload", "-r", "pypi", tarball])
+
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ Version 1.0.0 (May 23, 2020)
 Copyright (c) 2020 Shujia Huang
 """
 import os
+from argparse import Namespace
 
 try:
     from setuptools import setup, find_packages
@@ -13,32 +14,39 @@ except ImportError:
 
 
 DESCRIPTION = "ilus: A handy tools for generating WGS/WES analysis pipeline."
-DISTNAME = 'ilus'
-MAINTAINER = 'Shujia Huang'
-MAINTAINER_EMAIL = 'huangshujia9@gmail.com'
-URL = 'https://github.com/ShujiaHuang/ilus'
-LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/ShujiaHuang/ilus'
-VERSION = "1.0.2"
 
-ROOT_DIR = os.path.split(os.path.realpath(__file__))[0]
+meta = Namespace(
+    __DISTNAME__     = "ilus",
+    __AUTHOR__       = "Shujia Huang",
+    __AUTHOR_EMAIL__ = "huangshujia9@gmail.com",
+    __URL__          = "https://github.com/ShujiaHuang/ilus",
+    __LICENSE__      = "BSD (3-clause)",
+    __DOWNLOAD_URL__ = "https://github.com/ShujiaHuang/ilus",
+    __VERSION__      = "1.0.3",
+)
+
 
 if __name__ == "__main__":
-    setup(name=DISTNAME,
-          author=MAINTAINER,
-          author_email=MAINTAINER_EMAIL,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
+
+    THIS_PATH = os.path.abspath(os.path.dirname(__file__))
+    long_description = os.path.join(THIS_PATH, "README.md")
+
+    setup(name=meta.__DISTNAME__,
+          author=meta.__AUTHOR__,
+          author_email=meta.__AUTHOR_EMAIL__,
+          maintainer=meta.__AUTHOR__,
+          maintainer_email=meta.__AUTHOR_EMAIL__,
           description=DESCRIPTION,
-          long_description=(open(ROOT_DIR + "/README.rst").read()),
-          license=LICENSE,
-          url=URL,
-          download_url=DOWNLOAD_URL,
+          long_description=(open(long_description).read()),
+          long_description_content_type="text/markdown",
+          license=meta.__LICENSE__,
+          url=meta.__URL__,
+          download_url=meta.__DOWNLOAD_URL__,
+          version=meta.__VERSION__,
           packages=find_packages(),
           install_requires=[
               'PyYAML>=5.1.2',
           ],
-          version=VERSION,
           include_package_data=True,
           # scripts = [],
           entry_points={
