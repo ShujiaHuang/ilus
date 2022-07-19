f34795a8798d07c1d1c6bd589611273f417a02e1
Shujia Huang <huangshujia9@gmail.com>
2022-07-19 17:24:26 +0800

    improve and merge the codes

--- scripts/check_jobs_status.py
+++ scripts/check_jobs_status.py
@@ -2,46 +2,13 @@
 Author: Shujia Huang
 Date: 2020-04-25
 """
-import re
-import os
-import sys
-
-# Match anything looks like: "[xx] xxx done" or "[xx] xx xxx ... done"
-pattern = re.compile(r'^\[\S+\]\s+\S+.*?\s+done$')
-
-
-def check(input_fname):
-    is_finish = False
-    if os.path.exists(input_fname):
-        with open(input_fname) as I:
-            last_line = ""
-            for line in I:
-                last_line = line.strip()
-
-            match = pattern.match(last_line)
-            if match:
-                is_finish = True
-
-    return is_finish
-
+import argparse
+from ilus.utils import check_jobs_status
 
 if __name__ == "__main__":
+    cmdparser = argparse.ArgumentParser(description="Check the jobs have finished or not.")
+    cmdparser.add_argument("-I", "--input", dest="input", required=True,
+                           help="Input the log file of task.")
 
-    task_log_file = sys.argv[1]
-
-    is_all_finish = True
-    with open(task_log_file) as I:
-        for line in I:
-            # sample log_file task_shell_file"
-            if line.startswith("#"):
-                continue
-
-            sample, log_fname, task_shell = line.strip().split()
-            is_finish = check(log_fname)
-
-            if not is_finish:
-                is_all_finish = False
-                print ("[unfinish]\t%s\t%s" % (sample, task_shell))
-
-    if is_all_finish:
-        sys.stderr.write("** All Jobs done **\n")
+    args = cmdparser.parse_args()
+    check_jobs_status(args)
--- scripts/yhbatch_slurm_jobs.py
+++ scripts/yhbatch_slurm_jobs.py
@@ -1,14 +1,9 @@
 """Create yhbatch jobs in GuangZhou TianHe2 computer cluster.
 """
-import os
-import stat
 import argparse
-
-from datetime import datetime
-
+from ilus.utils import split_jobs
 
 if __name__ == "__main__":
-
     cmdparser = argparse.ArgumentParser(description="yhbatch jobs")
     cmdparser.add_argument("-I", "--input", dest="input", required=True,
                            help="Input shell file.")
@@ -18,60 +13,4 @@ if __name__ == "__main__":
                            help="The number of parallel tasks.")
 
     args = cmdparser.parse_args()
-
-    commands = []
-    with open(args.input) as I:
-        for line in I:
-            if line.startswith("#"):
-                continue
-
-            commands.append(line.strip())
-
-    total_cmd_num = len(commands)
-    if args.number > total_cmd_num:
-        args.number = total_cmd_num
-
-    cmd_num_perjob = total_cmd_num // args.number
-    if total_cmd_num % args.number > 0:
-        cmd_num_perjob += 1
-
-    _, shell_fname = os.path.split(args.input)
-    now_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    sub_shell_dir = os.path.abspath("{shell_fname}.{now_time}".format(**locals()))
-    if not os.path.exists(sub_shell_dir):
-        os.makedirs(sub_shell_dir)
-
-    part_num = cmd_num_perjob // args.t
-    if cmd_num_perjob % args.t > 0:
-        part_num += 1
-
-    for i, k in enumerate(range(0, total_cmd_num, cmd_num_perjob)):
-        sub_shell_fname = os.path.join(sub_shell_dir, "work.%d.sh" % (i+1))
-        with open(sub_shell_fname, "w") as OUT:
-
-            n = 0
-            OUT.write("#!/bin/bash\n")
-            for j, cmd in enumerate(commands[k:k+cmd_num_perjob]):
-                OUT.write("%s &\n" % cmd if args.t > 1 else "%s\n" % cmd)
-                if (j + 1) % args.t == 0:
-                    n += 1
-                    if args.t > 1:
-                        OUT.write("wait\n")
-                    OUT.write("echo \"----------- %d/%d ----------\"\n" % (n, part_num))
-
-            if (j + 1) % args.t > 0:
-                OUT.write("wait\n")
-                OUT.write("echo \"----------- %d/%d ----------\"\n" % (n+1, part_num))
-
-        os.chmod(sub_shell_fname, stat.S_IRWXU)  # 0700
-
-
-
-
-
-
-
-
-
-
-
+    split_jobs(args)
