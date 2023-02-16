8481f5bce9a18e15c7c187642a8f51dffd0e0544
Shujia Huang <huangshujia9@gmail.com>
2023-02-16 11:10:49 +0800

    命令行参数优化

--- ilus/main.py
+++ ilus/main.py
@@ -16,92 +16,255 @@ from datetime import datetime
 from ilus.pipeline.wgs import wgs, genotypeGVCFs, variantrecalibrator
 from ilus.utils import split_jobs, check_jobs_status
 
-VERSION = "1.2.2"
+VERSION = "1.3.0"
+
+
+def create_wgs_pipeline_command(commands):
+    """Add arguments to create WGS pipeline command."""
+    wgs_cmd = commands.add_parser(
+        "WGS",
+        help="Create a pipeline for WGS (from FASTQ to genotype VCF)."
+    )
+
+    wgs_cmd.add_argument(
+        "-n", "--name",
+        dest="project_name",
+        type=str,
+        default="test",
+        help="Name of the project. Default value: test"
+    )
+
+    wgs_cmd.add_argument(
+        "-C", "--conf",
+        dest="sysconf",
+        required=True,
+        help="YAML configuration file specifying system details."
+    )
+
+    wgs_cmd.add_argument(
+        "-L", "--fastqlist",
+        dest="fastqlist",
+        type=str,
+        required=True,
+        help="List of alignment FASTQ files."
+    )
+
+    wgs_cmd.add_argument(
+        "-O", "--outdir",
+        dest="outdir",
+        required=True,
+        help="Output directory for results."
+    )
+
+    wgs_cmd.add_argument(
+        "-c", "--cram",
+        dest="cram",
+        action="store_true",
+        help="Convert BAM to CRAM after BQSR and save alignment file storage."
+    )
+
+    wgs_cmd.add_argument(
+        "-P", "--process",
+        dest="wgs_processes",
+        type=str,
+        default="align,markdup,BQSR,gvcf,combineGVCFs,genotype,VQSR",
+        help="Specify one or more processes (separated by comma) of WGS pipeline. "
+             "Possible values: {align,markdup,BQSR,gvcf,combineGVCFs,genotype,VQSR}"
+    )
+
+    wgs_cmd.add_argument(
+        "-f", "--force-overwrite",
+        dest="overwrite",
+        action="store_true",
+        help="Force overwrite existing shell scripts and folders."
+    )
+
+    # Todo: Write a dry run function for testing the pipeline without truely run the pipeline.
+    wgs_cmd.add_argument(
+        "-dr", "--dry-run",
+        dest="dry_run",
+        action="store_true",
+        help="Dry run the pipeline for testing."
+    )
+
+    return wgs_cmd
+
+
+def create_genotype_joint_calling_command(commands):
+    """Add arguments to create genotype joint calling command."""
+    genotype_cmd = commands.add_parser(
+        "genotype-joint-calling",
+        help="Genotype from GVCFs."
+    )
+
+    genotype_cmd.add_argument(
+        "-C", "--conf",
+        dest="sysconf",
+        required=True,
+        help="YAML configuration file specifying system details."
+    )
+
+    genotype_cmd.add_argument(
+        "-L", "--gvcflist",
+        dest="gvcflist",
+        type=str,
+        required=True,
+        help="List of GVCF files. One gvcf_file per-row and the format should looks like: "
+             "[interval\tgvcf_file_path]. Column [1] is a symbol which could represent "
+             "the genome region of the gvcf_file and column [2] should be the path."
+    )
+
+    genotype_cmd.add_argument(
+        "-O", "--outdir",
+        dest="outdir",
+        required=True,
+        help="A directory for output results."
+    )
+
+    genotype_cmd.add_argument(
+        "-n", "--name",
+        dest="project_name",
+        type=str,
+        default="test",
+        help="Name of the project. [test]"
+    )
+
+    genotype_cmd.add_argument(
+        "--as_pipe_shell_order",
+        dest="as_pipe_shell_order",
+        action="store_true",
+        help="Keep the shell name as the order of `WGS`."
+    )
+
+    genotype_cmd.add_argument(
+        "-f", "--force",
+        dest="overwrite",
+        action="store_true",
+        help="Force overwrite existing shell scripts and folders."
+    )
+
+    return genotype_cmd
+
+
+def create_vqsr_command(commands):
+    """Add arguments to create VQSR command."""
+    vqsr_cmd = commands.add_parser("VQSR", help="VQSR")
+    vqsr_cmd.add_argument(
+        "-C", "--conf",
+        dest="sysconf",
+        required=True,
+        help="YAML configuration file specifying details about system."
+    )
+
+    vqsr_cmd.add_argument(
+        "-L", "--vcflist",
+        dest="vcflist",
+        type=str,
+        required=True,
+        help="VCFs file list. One file per-row."
+    )
+
+    vqsr_cmd.add_argument(
+        "-O", "--outdir",
+        dest="outdir",
+        required=True,
+        help="A directory for output results."
+    )
+
+    vqsr_cmd.add_argument(
+        "-n", "--name",
+        dest="project_name",
+        type=str,
+        default="test",
+        help="Name of the project. [test]"
+    )
+
+    vqsr_cmd.add_argument(
+        "--as_pipe_shell_order",
+        dest="as_pipe_shell_order",
+        action="store_true",
+        help="Keep the shell name as the order of `WGS`."
+    )
+
+    vqsr_cmd.add_argument(
+        "-f", "--force",
+        dest="overwrite",
+        action="store_true",
+        help="Force overwrite existing shell scripts and folders."
+    )
+
+    return vqsr_cmd
+
+
+def create_utility_module_command(commands):
+    split_job_cmd = commands.add_parser("split-jobs", help="Split the whole shell into multiple jobs.")
+
+    split_job_cmd.add_argument(
+        "-I", "--input",
+        dest="input",
+        required=True,
+        help="Input shell file."
+    )
+
+    split_job_cmd.add_argument(
+        "-p", "--prefix",
+        dest="prefix",
+        type=str,
+        default="work",
+        help="The prefix name of output sub-shell file. [work]"
+    )
+
+    split_job_cmd.add_argument(
+        "-n", "--number",
+        dest="number",
+        type=int,
+        required=True,
+        help="The number of sub tasks (shells)."
+    )
+
+    split_job_cmd.add_argument(
+        "-t", "--parallel",
+        dest="t",
+        type=int,
+        required=True,
+        help="The number of parallel jobs."
+    )
+
+    check_job_cmd = commands.add_parser("check-jobs", help="Check the jobs have finished or not.")
+    check_job_cmd.add_argument(
+        "-I", "--input",
+        dest="input",
+        required=True,
+        help="Input the log file of task."
+    )
+
+    return
 
 
 def parse_commandline_args():
     """Parse input commandline arguments, handling multiple cases.
     """
-    desc = "ilus (Version = %s): A WGS/WES analysis pipeline generator." % VERSION
+    desc = f"ilus (Version = {VERSION}): A WGS/WES analysis pipeline generator."
     cmdparser = argparse.ArgumentParser(description=desc)
-    cmdparser.add_argument("-v", "--version", action="store_true", help="show the version of ilus.")
+    cmdparser.add_argument(
+        "-v", "--version",
+        action="store_true",
+        help="show the version of ilus and exit."
+    )
 
     commands = cmdparser.add_subparsers(dest="command", title="ilus commands")
 
-    # The standard pipeline for WGS.
-    pipeline_cmd = commands.add_parser("WGS", help="Creating pipeline for WGS(from fastq to genotype VCF)")
-    pipeline_cmd.add_argument("-n", "--name", dest="project_name", type=str, default="test",
-                              help="Name of the project. Default value: test")
-    pipeline_cmd.add_argument("-C", "--conf", dest="sysconf", required=True,
-                              help="YAML configuration file specifying details about system.")
-    pipeline_cmd.add_argument("-L", "--fastqlist", dest="fastqlist", type=str, required=True,
-                              help="The input file list of alignment FASTQ Files.")
-    pipeline_cmd.add_argument("-O", "--outdir", dest="outdir", required=True,
-                              help="A directory for output results.")
+    # The arguments for WGS.
+    create_wgs_pipeline_command(commands)
 
-    pipeline_cmd.add_argument("-c", "--cram", dest="cram", action="store_true",
-                              help="Covert BAM to CRAM after BQSR and save alignment file storage.")
-    pipeline_cmd.add_argument("-P", "--Process", dest="wgs_processes", type=str,
-                              help="Specific one or more processes (separated by comma) of WGS pipeline. "
-                                   "Defualt value: align,markdup,BQSR,gvcf,combineGVCFs,genotype,VQSR. "
-                                   "Possible values: {align,markdup,BQSR,gvcf,combineGVCFs,genotype,VQSR}",
-                              default="align,markdup,BQSR,gvcf,combineGVCFs,genotype,VQSR")
+    # The arguments for GVCFs
+    create_genotype_joint_calling_command(commands)
 
-    pipeline_cmd.add_argument("-f", "--force_overwrite", dest="overwrite", action="store_true",
-                              help="Force overwrite existing shell scripts and folders.")
-
-    # Todo: Write a dry run function for testing the pipeline without truely run the pipeline.
-    pipeline_cmd.add_argument("-dr", "--dry-run", dest="dry_run", action="store_true",
-                              help="Dry run for testing the pipeline.")
-
-    # Genotype from GVCFs
-    genotype_cmd = commands.add_parser("genotype-joint-calling", help="Genotype from GVCFs.")
-    genotype_cmd.add_argument("-C", "--conf", dest="sysconf", required=True,
-                              help="YAML configuration file specifying details about system.")
-    genotype_cmd.add_argument("-L", "--gvcflist", dest="gvcflist", type=str, required=True,
-                              help="GVCFs file list. One gvcf_file per-row and the format should looks like: "
-                                   "[interval\tgvcf_file_path]. Column [1] is a symbol which could represent "
-                                   "the genome region of the gvcf_file and column [2] should be the path.")
-    genotype_cmd.add_argument("-O", "--outdir", dest="outdir", required=True,
-                              help="A directory for output results.")
-
-    genotype_cmd.add_argument("-n", "--name", dest="project_name", type=str, default="test",
-                              help="Name of the project. [test]")
-    genotype_cmd.add_argument("--as_pipe_shell_order", dest="as_pipe_shell_order", action="store_true",
-                              help="Keep the shell name as the order of `WGS`.")
-    genotype_cmd.add_argument("-f", "--force", dest="overwrite", action="store_true",
-                              help="Force overwrite existing shell scripts and folders.")
-
-    # Genotype from VQSR
-    vqsr_cmd = commands.add_parser("VQSR", help="VQSR")
-    vqsr_cmd.add_argument("-C", "--conf", dest="sysconf", required=True,
-                          help="YAML configuration file specifying details about system.")
-    vqsr_cmd.add_argument("-L", "--vcflist", dest="vcflist", type=str, required=True,
-                          help="VCFs file list. One file per-row.")
-    vqsr_cmd.add_argument("-O", "--outdir", dest="outdir", required=True,
-                          help="A directory for output results.")
-
-    vqsr_cmd.add_argument("-n", "--name", dest="project_name", type=str, default="test",
-                          help="Name of the project. [test]")
-    vqsr_cmd.add_argument("--as_pipe_shell_order", dest="as_pipe_shell_order", action="store_true",
-                          help="Keep the shell name as the order of `WGS`.")
-    vqsr_cmd.add_argument("-f", "--force", dest="overwrite", action="store_true",
-                          help="Force overwrite existing shell scripts and folders.")
+    # The arguments for VQSR
+    create_vqsr_command(commands)
 
     # Utility tools
-    split_job_cmd = commands.add_parser("split-jobs", help="Split the whole shell into multiple jobs.")
-    split_job_cmd.add_argument("-I", "--input", dest="input", required=True,
-                               help="Input shell file.")
-    split_job_cmd.add_argument("-p", "--prefix", dest="prefix", type=str, default="work",
-                               help="The prefix name of output sub-shell file. [work]")
-    split_job_cmd.add_argument("-n", "--number", dest="number", type=int, required=True,
-                               help="The number of sub tasks (shells).")
-    split_job_cmd.add_argument("-t", "--parallel", dest="t", type=int, required=True,
-                               help="The number of parallel jobs.")
-
-    check_job_cmd = commands.add_parser("check-jobs", help="Check the jobs have finished or not.")
-    check_job_cmd.add_argument("-I", "--input", dest="input", required=True,
-                               help="Input the log file of task.")
+    create_utility_module_command(commands)
 
     return cmdparser.parse_args()
 
