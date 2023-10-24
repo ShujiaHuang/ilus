a3b96578ed1af921f6ef5e7b3674adeba415cda9
Shujia Huang <huangshujia9@gmail.com>
2023-10-24 11:08:48 +0800

    check_samplesheet -> check_input_sheet

--- ilus/modules/utils/__init__.py
+++ ilus/modules/utils/__init__.py
@@ -1,4 +1,4 @@
 from ._utils import safe_makedir, file_exists
-from ._check_samplesheet import check_input_sheet
+from ._check_input import check_input_sheet
 from ._split_jobs import split_jobs
 from ._check_jobs import check_jobs_status
