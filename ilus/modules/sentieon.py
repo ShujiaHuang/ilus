5ee94b1aa412cfae818d57fb0c924da5c4638a94
Shujia Huang <huangshujia9@gmail.com>
2023-11-01 17:18:00 +0800

    fix parameters

--- ilus/modules/sentieon.py
+++ ilus/modules/sentieon.py
@@ -33,6 +33,8 @@ class Sentieon(object):
         self.sentieon = self.config['sentieon']['sentieon']
         self.reference_index = self.config["resources"]["reference"]
         self.reference_fasta = self.config["resources"]["reference"]
+        self.driver_options = " ".join(
+            [str(x) for x in self.config["sentieon"].get("sentieon_driver_options", [])])
 
     def bwamem(self, out_prefix, rgID, fastq1, fastq2=""):
         """Mapping reads with BWA-MEM and sorting BAM to coordinate.
@@ -45,10 +47,10 @@ class Sentieon(object):
             fastq2 = ""
 
         bwa_options = " ".join([str(x) for x in self.config["sentieon"].get("bwamem_options", [])])
-        sort_options = " ".join([str(x) for x in self.config["sentieon"].get("sort_options", [])])
-
+        util_sort_options = " ".join(
+            [str(x) for x in self.config["sentieon"].get("util_sort_options", [])])
         bwa_cmd = (f"time {self.sentieon} bwa mem {bwa_options} -R {rgID} {self.reference_index} "
-                   f"{fastq1} {fastq2} | {self.sentieon} util sort {sort_options} --sam2bam "
+                   f"{fastq1} {fastq2} | {self.sentieon} util sort {util_sort_options} --sam2bam "
                    f"-o {out_prefix}.sorted.bam -i -")
 
         return f"{out_prefix}.sorted.bam", bwa_cmd
@@ -56,19 +58,21 @@ class Sentieon(object):
     def alignment_metrics(self, input_bam, out_prefix):
         """ Alignment metrics. Call this function before `markduplicates`
         """
-        metrics_options = " ".join([str(x) for x in self.config["sentieon"].get("alignment_metrics", [])])
         adapter_seq = ",".join([str(x) for x in self.config["resources"].get("adapter_sequence", [])])
         if len(adapter_seq) == 0:
             adapter_seq = "' '"
 
-        align_metrics_cmd = (f"time {self.sentieon} driver -r {self.reference_fasta} {metrics_options} "
+        coverage_options = " ".join([
+            str(x) for x in self.config["sentieon"].get("coverage_options", [])])
+        align_metrics_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                             f"-r {self.reference_fasta} "
                              f"-i {input_bam} "
                              f"--algo MeanQualityByCycle {out_prefix}.mq_metrics.txt "
                              f"--algo QualDistribution {out_prefix}.qd_metrics.txt "
                              f"--algo GCBias --summary {out_prefix}.gc_summary.txt {out_prefix}.gc_metrics.txt "
                              f"--algo AlignmentStat --adapter_seq {adapter_seq} {out_prefix}.aln_metrics.txt "
                              f"--algo InsertSizeMetricAlgo {out_prefix}.is_metrics.txt "
-                             f"--algo CoverageMetrics --omit_base_output {out_prefix}.coverage_metrics")
+                             f"--algo CoverageMetrics {coverage_options} {out_prefix}.coverage_metrics")
 
         plot_cmd = (f"{self.sentieon} plot GCBias -o {out_prefix}.gc-report.pdf {out_prefix}.gc_metrics.txt && "
                     f"{self.sentieon} plot QualDistribution -o {out_prefix}.qd-report.pdf {out_prefix}.qd_metrics.txt && "
@@ -88,25 +92,26 @@ class Sentieon(object):
         output_markdup_bam = str(output_markdup_bam)
         score_info_file = output_markdup_bam.replace(".bam", ".score.txt")
         dedup_metrics_f = output_markdup_bam.replace(".bam", ".metrics.txt")
-        dedup_cmd = (f"time {self.sentieon} driver {locus_collector_options} "
+        dedup_cmd = (f"time {self.sentieon} driver {self.driver_options} "
                      f"-i {input_bam} "
-                     f"--algo LocusCollector --fun score_info {score_info_file} && "
-                     f"time {self.sentieon} driver {dedup_options} "
+                     f"--algo LocusCollector {locus_collector_options} "
+                     f"--fun score_info {score_info_file} && "
+                     f"time {self.sentieon} driver {self.driver_options} "
                      f"-i {input_bam} "
-                     f"--algo Dedup --score_info {score_info_file} "
+                     f"--algo Dedup {dedup_options} --score_info {score_info_file} "
                      f"--metrics {dedup_metrics_f} {output_markdup_bam}")
 
         coverage_options = " ".join([
-            str(x) for x in self.config["sentieon"].get("coverage_options", [])
-        ])
+            str(x) for x in self.config["sentieon"].get("coverage_options", [])])
 
         cvg_metrics_fn = output_markdup_bam.replace(".bam", ".coverage_metrics")
 
         # '--omit_base_output' skip the output of the per locus coverage with no partition.
         # This option can be used when you do not use intervals to save space.
-        cvg_cmd = (f"{self.sentieon} driver -r {self.reference_fasta} {coverage_options} "
+        cvg_cmd = (f"{self.sentieon} driver {self.driver_options} "
+                   f"-r {self.reference_fasta} "
                    f"-i {output_markdup_bam} "
-                   f"--algo CoverageMetrics --omit_base_output {cvg_metrics_fn}")
+                   f"--algo CoverageMetrics {coverage_options} {cvg_metrics_fn}")
 
         return dedup_cmd + " && " + cvg_cmd
 
@@ -118,8 +123,10 @@ class Sentieon(object):
         known_Mills_indels = self.config["resources"]["bundle"]["mills"]
         known_1000G_indels = self.config["resources"]["bundle"]["1000G_known_indel"]
 
-        return (f"time {self.sentieon} driver -r {self.reference_fasta} {indel_realigner_options} "
-                f"-i {input_bam} --algo Realigner "
+        return (f"time {self.sentieon} driver {self.driver_options} "
+                f"-r {self.reference_fasta} "
+                f"-i {input_bam} "
+                f"--algo Realigner {indel_realigner_options} "
                 f"-k {known_Mills_indels} "
                 f"-k {known_1000G_indels} {output_realig_bam}")
 
@@ -136,21 +143,23 @@ class Sentieon(object):
         known_1000G_indels = self.config["resources"]["bundle"]["1000G_known_indel"]
         bqsr_recaltable_options = " ".join([str(x) for x in self.config["sentieon"].get(
             "bqsr_recaltable_options", [])])
-        bqsr_apply_options = " ".join([str(x) for x in self.config["sentieon"].get(
-            "bqsr_apply_options", [])])
 
         # Create recalibrate table file for BQSR
-        recal_table_cmd = (f"time {self.sentieon} driver "
-                           f"-r {self.reference_fasta} {bqsr_recaltable_options} "
-                           f"-i {input_bam} --algo QualCal "
+        recal_table_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                           f"-r {self.reference_fasta} "
+                           f"-i {input_bam} "
+                           f"--algo QualCal {bqsr_recaltable_options} "
                            f"-k {dbsnp} "
                            f"-k {known_Mills_indels} "
                            f"-k {known_1000G_indels} {out_bqsr_recal_table}")
 
         # Apply BQSR
         recal_table_post_fn = f"{out_bqsr_recal_table}.post"
-        apply_bqsr_cmd = (f"time {self.sentieon} driver -r {self.reference_fasta} {bqsr_apply_options} "
-                          f"-i {input_bam} -q {out_bqsr_recal_table} --algo QualCal "
+        apply_bqsr_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                          f"-r {self.reference_fasta} "
+                          f"-i {input_bam} "
+                          f"-q {out_bqsr_recal_table} "
+                          f"--algo QualCal {bqsr_recaltable_options} "
                           f"-k {dbsnp} "
                           f"-k {known_Mills_indels} "
                           f"-k {known_1000G_indels} {recal_table_post_fn} "
@@ -164,10 +173,12 @@ class Sentieon(object):
         # Plot report
         recal_report_table = f"{out_bqsr_recal_table}.report.csv"
         bqsrreport_plot_fn = f"{out_bqsr_recal_table}.bqsrreport.pdf"
-        plot_cmd = (f"time {self.sentieon} driver --algo QualCal --plot "
+        plot_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                    f"--algo QualCal --plot "
                     f"--before {out_bqsr_recal_table} "
                     f"--after {recal_table_post_fn} {recal_report_table} && "
-                    f"{self.sentieon} plot bqsr -o {bqsrreport_plot_fn} {recal_report_table}")
+                    f"{self.sentieon} plot QualCal "
+                    f"-o {bqsrreport_plot_fn} {recal_report_table}")
 
         return recal_table_cmd + " && " + apply_bqsr_cmd + " && " + plot_cmd
 
@@ -179,50 +190,41 @@ class Sentieon(object):
         :param output_vcf_fname: gvcf
         :param interval: Interval string or file (BED/Picard)
         :return:
-
-        'emit_mode' in HaplotypeCaller: determines what calls will be emitted. Possible values for mode are:
-              - variant: emit calls only at confident variant sites. This is the default behavior.
-              - confident: emit calls at confident variant sites or confident reference sites.
-              - all: emit all calls, regardless of their confidence.
-              - gvcf: emits additional information required for joint calling.
-                      This option is required if you want to perform joint calling using the
-                      GVCFtyper algorithm.
         """
         dbsnp = self.config["resources"]["bundle"]["dbsnp"]
         hc_options = " ".join([str(x) for x in self.config["sentieon"].get("hc_options", [])])
 
-        if interval:
-            hc_options += f" --interval {interval}"
-
-        if ".g.vcf" not in str(output_vcf_fname):
+        if ("gvcf" in hc_options) and (".g.vcf" not in str(output_vcf_fname)):
             raise ValueError(f"[ERROR] {output_vcf_fname} missing .g.vcf in file name")
 
-        return (f"time {self.sentieon} driver -r {self.reference_fasta} {hc_options} "
+        driver_options = f"{self.driver_options} --interval {interval}" \
+            if interval else self.driver_options
+
+        return (f"time {self.sentieon} driver {driver_options} "
+                f"-r {self.reference_fasta} "
                 f"-i {input_bam} "
                 f"-q {bqsr_recal_table} "
-                f"--algo Haplotyper -d {dbsnp} "
-                f"--emit_mode gvcf {output_vcf_fname}")
+                f"--algo Haplotyper {hc_options} "
+                f"-d {dbsnp} {output_vcf_fname}")
 
     def genotypeGVCFs(self, input_gvcfs_list, output_vcf_fname, interval=None):
         """Perform the joint calling by `GVCFtyper` module with input GVCFs.
-
-        'emit_mode' in GVCFtyper: determines what calls will be emitted. Possible values for mode are:
-              - variant: emit calls only at confident variant sites. This is the default behavior.
-              - confident: emit calls at confident variant sites or confident reference sites.
-              - all: emit all calls, regardless of their confidence.
         """
         # location of the Single Nucleotide Polymorphism database (dbSNP) used to
         # label known variants.
         dbsnp = self.config["resources"]["bundle"]["dbsnp"]
         gvcftyper_options = " ".join([
-            str(x) for x in self.config["sentieon"].get("gvcftyper_options", [])
-        ])
-        if interval:
-            gvcftyper_options += f" --interval {interval}"
+            str(x) for x in self.config["sentieon"].get("gvcftyper_options", [])])
+
+        driver_options = f"{self.driver_options} --interval {interval}" \
+            if interval else self.driver_options
 
         in_gvcf_arguments = " ".join([f"-v {gvcf_fn}" for gvcf_fn in input_gvcfs_list])
-        return (f"time {self.sentieon} driver -r {self.reference_fasta} {gvcftyper_options} "
-                f"--algo GVCFtyper {in_gvcf_arguments} -d {dbsnp} {output_vcf_fname}")
+        return (f"time {self.sentieon} driver {driver_options} "
+                f"-r {self.reference_fasta}  "
+                f"--algo GVCFtyper {gvcftyper_options} "
+                f"-d {dbsnp} "
+                f"{in_gvcf_arguments} {output_vcf_fname}")
 
     def variantrecalibrator(self, input_vcf, output_vcf_fname):
         """Use VarCal and ApplyVarCal modules to do VQSR in sentieon.
@@ -251,47 +253,51 @@ class Sentieon(object):
         out_prefix = str(output_vcf_fname).replace(".gz", "").replace(".vcf", "")  # delete .vcf.gz
         out_snp_vqsr_fname = out_prefix + ".SNPs.vcf.gz"
         snp_vqsr_cmd = (
-            f"time {self.sentieon} driver -r {self.reference_fasta} --algo VarCal "
+            f"time {self.sentieon} driver {self.driver_options} "
+            f"-r {self.reference_fasta} "
+            f"--algo VarCal {vqsr_options} "
             f"-v {input_vcf} "
             f"--resource {resource_hapmap} --resource_param hapmap,known=false,training=true,truth=true,prior=15.0  "
             f"--resource {resource_omni} --resource_param omini,known=false,training=true,truth=truth,prior=12.0 "
             f"--resource {resource_1000G} --resource_param 1000G,known=false,training=true,truth=false,prior=10.0 "
             f"--resource {resource_dbsnp} --resource_param dbsnp,known=true,training=false,truth=false,prior=2.0 "
-            f"{vqsr_options} "
             f"--var_type SNP "
             f"--max-gaussians {self.config['sentieon']['vqsr_snp_max_gaussians']} "
             f"--tranches_file {out_prefix}.SNPs.tranches.csv "
             f"--plot_file {out_prefix}.plot.SNPs.csv "
             f"{out_prefix}.SNPs.recal"  # Output quality recalibrator data for SNPs
         )
-        apply_snp_vqsr_cmd = (f"time {self.sentieon} driver -r {self.reference_fasta} "
-                              f"--algo ApplyVarCal -v {input_vcf} "
+        apply_snp_vqsr_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                              f"-r {self.reference_fasta} "
+                              f"--algo ApplyVarCal {apply_snp_vqsr_options} "
+                              f"-v {input_vcf} "
                               f"--var_type SNP "
                               f"--recal {out_prefix}.SNPs.recal "
                               f"--tranches_file {out_prefix}.SNPs.tranches.csv "
-                              f"{apply_snp_vqsr_options} "
                               f"{out_snp_vqsr_fname}")
 
         # Indel VQSR after SNP
         indel_vqsr_cmd = (
-            f"time {self.sentieon} driver -r {self.reference_fasta} --algo VarCal"
+            f"time {self.sentieon} driver {self.driver_options} "
+            f"-r {self.reference_fasta} "
+            f"--algo VarCal {vqsr_options} "
             f"-v {out_snp_vqsr_fname} "
             f"--resource {resource_mills_gold_indels} --resource_param omini,known=false,training=true,truth=truth,prior=12.0 "
             f"--resource {resource_1000G_known_indel} --resource_param 1000G,known=false,training=true,truth=true,prior=10.0 "
             f"--resource {resource_dbsnp} --resource_param dbsnp,known=true,training=false,truth=false,prior=2.0 "
-            f"{vqsr_options} "
             f"--var_type INDEL "
             f"--max-gaussians {self.config['sentieon']['vqsr_indel_max_gaussians']} "
             f"--tranches_file {out_prefix}.INDELs.tranches.csv "
             f"--plot_file {out_prefix}.plot.INDELs.csv "
             f"{out_prefix}.INDELs.recal"  # Output quality recalibrator data for INDELs
         )
-        apply_indel_vqsr_cmd = (f"time {self.sentieon} driver -r {self.reference_fasta} "
-                                f"--algo ApplyVarCal -v {out_snp_vqsr_fname} "
+        apply_indel_vqsr_cmd = (f"time {self.sentieon} driver {self.driver_options} "
+                                f"-r {self.reference_fasta} "
+                                f"--algo ApplyVarCal {apply_indel_vqsr_options} "
+                                f"-v {out_snp_vqsr_fname} "
                                 f"--var_type INDEL "
                                 f"--recal {out_prefix}.INDELs.recal "
                                 f"--tranches_file {out_prefix}.INDELs.tranches.csv "
-                                f"{apply_indel_vqsr_options} "
                                 f"{output_vcf_fname} && rm -f {out_snp_vqsr_fname}")
 
         return " && ".join([snp_vqsr_cmd, apply_snp_vqsr_cmd, indel_vqsr_cmd, apply_indel_vqsr_cmd])
