ebbb222abef5373adc942349fbd8c5a4aec5872b
Shujia Huang <huangshujia9@gmail.com>
2023-11-22 10:30:51 +0800

    add a comment

--- ilus/launch/runfunction.py
+++ ilus/launch/runfunction.py
@@ -549,6 +549,7 @@ def run_variantrecalibrator(kwargs, out_folder_name: str, aione: dict = None, is
                                    aione["genotype_vcf_list"],
                                    combine_vcf_fname)
         cmd.append(concat_vcf_cmd)
+        # 只有多于一个 gvcf 需要 concat，才需要删除子文件
         if "is_rm_sub_vcf_after_concat" in aione and aione["is_rm_sub_vcf_after_concat"]:
             cmd.append(f"rm -f {' '.join(aione['genotype_vcf_list'])}")
             cmd.append(f"rm -f {' '.join([f + '.tbi' for f in aione['genotype_vcf_list']])}")
